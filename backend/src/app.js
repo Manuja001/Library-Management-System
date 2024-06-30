a406a69a88df0a4c5cad3805f023385d1756e215
Manuja Sandeep <it22221100my.sliit.lk>
2024-06-30 17:28:23 +0530

    server is up and running

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json({ limit: "20mb" }));
+
+app.get("/", (req, res, next) => {
+  res.send("<h2>Library Management System API</h2>");
+  next();
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
